fb8c53fc5bd685c53a18b665b33c9f157bc08213
Grace Mwendwa <122599984+grace-mwendwa@users.noreply.github.com>
2024-11-12 15:20:34 +0300

    Added search functionality using Api

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-function currentWeatherInput(event) {
-  event.preventDefault();
-
+function searchWeather(response) {
+  let temperatureElement = document.querySelector("#temperatureNumeric");
+  let temperatureResult = Math.round(response.data.temperature.current);
   let currentCity = document.querySelector("#weatherCurrentCity");
-  let currentCityInput = document.querySelector("#searchCityInput");
 
-  currentCity.innerHTML = currentCityInput.value;
+  currentCity.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperatureResult;
 }
 
-let searchCityInput = document.querySelector("#weatherSearchForm");
-searchCityInput.addEventListener("submit", currentWeatherInput);
+function searchCity(city) {
+  let apiKey = "64a733e6o87ac4af3690142d20281tdb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(searchWeather);
+}
+
+function currentWeatherResult(event) {
+  event.preventDefault();
+
+  let currentCityInput = document.querySelector("#searchCityInput");
+
+  searchCity(currentCityInput.value);
+}
+
+let searchCityWeather = document.querySelector("#weatherSearchForm");
+
+searchCityWeather.addEventListener("submit", currentWeatherResult);
+
+searchCity("Nairobi");
